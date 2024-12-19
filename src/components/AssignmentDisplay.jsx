f69be419574d30e2e4b4ca6d420fc10acd38458c
MaathildeEtu <mathilde.decoux@etu.unilim.fr>
2024-12-19 16:55:22 +0100

    Main

--- src/components/AssignmentDisplay.jsx
+++ src/components/AssignmentDisplay.jsx
@@ -2,7 +2,7 @@
 // Il prend en props le tableau d'assignments
 export function AssignmentDisplay({ assignments }) {
   return (
-    <ul className="space-y-2">
+    <ul className="space-y-2 flex flex-col items-center">
       {assignments.map((assignment, index) => (
         <li key={index} className="text-white font-extralight">
           <span className="font-bold">{assignment.giver}</span> offre un beau cadeau à{" "}
